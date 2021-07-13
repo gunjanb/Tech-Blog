39ea29794db4020e36d92d91b65b860137208621
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-07-13 08:44:50 -0700

    updated for login and singup routes

--- controllers/viewRoutes/home-routes.js
+++ controllers/viewRoutes/home-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // when app runs home page displays all blog posts
 router.get("/", async (req, res) => {
@@ -40,33 +41,21 @@ router.get("/", async (req, res) => {
   }
 });
 
-// router.get("/signup", withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ["password"] },
-//       include: [{ model: Project }],
-//     });
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
 
-//     const user = userData.get({ plain: true });
+  res.render("login");
+});
 
-//     res.render("profile", {
-//       ...user,
-//       logged_in: true,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// router.get("/login", (req, res) => {
-//   // If the user is already logged in, redirect the request to another route
-//   if (req.session.logged_in) {
-//     res.redirect("/profile");
-//     return;
-//   }
-
-//   res.render("login");
-// });
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
+});
 
 module.exports = router;
