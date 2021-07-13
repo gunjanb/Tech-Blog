5a1cb3c464a35777a689b57fc2cdd74a1a996a73
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-07-13 07:36:27 -0700

    updated for auth

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+//  middleware to verify user  is logged in or not before giving access to a route
+const withAuth = (req, res, next) => {
+  if (!req.session.logged_in) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
