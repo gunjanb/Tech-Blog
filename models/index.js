80afde8412986806d4b2c3f5a70f467b3956f56f
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-07-13 07:42:12 -0700

    updated for comment fields

--- models/index.js
+++ models/index.js
@@ -2,14 +2,35 @@ const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
 
+//post
+//fk in comment
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+//fk in post
 Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
+//User
+//fk in  post
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+//fk in comment
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+//comment
+//fk in comment
 Comment.belongsTo(Post, {
   foreignKey: "post_id",
 });
 
+//fk in comment
 Comment.belongsTo(User, {
   foreignKey: "user_id",
 });
