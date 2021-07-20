8f96493d5d732ffde0a8a4d18a2d27d936f8838c
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-07-20 14:57:36 -0700

    updated for home routes and cleaned code

--- controllers/viewRoutes/home-routes.js
+++ controllers/viewRoutes/home-routes.js
@@ -29,9 +29,7 @@ router.get("/", async (req, res) => {
         },
       ],
     });
-    console.log(dbPostData);
     const posts = dbPostData.map((post) => post.get({ plain: true }));
-    console.log(posts);
     res.render("homepost", {
       posts,
       loggedIn: req.session.logged_in,
@@ -41,21 +39,97 @@ router.get("/", async (req, res) => {
   }
 });
 
+// GET /login  -- render login page
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/dashboard");
     return;
   }
-
   res.render("login");
 });
 
+// GET /signup -- render signup page
 router.get("/signup", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/");
     return;
   }
   res.render("signup");
+});
+
+//GET /post/id --render single post by id along with comments  asssociated with it
+router.get("/post/:id", withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "title", "content", "created_at"],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["name"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["id", "name"],
+        },
+      ],
+    });
+
+    if (!dbPostData) {
+      res.status(404).json({ message: "No post found with this id" });
+      return;
+    }
+    // serialize the data
+    const post = dbPostData.get({ plain: true });
+    // pass data to template
+    res.render("singlepost", {
+      post,
+      currentuser: req.session.user_id,
+      loggedIn: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET /comment/update/id --render update form for comment
+router.get("/comment/update/:id", async (req, res) => {
+  try {
+    const dbCommentData = await Comment.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "comment_text", "user_id", "post_id"],
+    });
+
+    if (!dbCommentData) {
+      res.status(404).json({ message: "No post found with this id" });
+      return;
+    }
+
+    // serialize the data
+    const commentData = dbCommentData.get({ plain: true });
+    // pass data to template
+    res.render("updatecomment", {
+      commentData,
+      currentuser: req.session.user_id,
+      loggedIn: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
