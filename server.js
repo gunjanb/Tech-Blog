85fd49a9a53d0b0877c8dbd6dde003418843e5cc
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-07-22 08:02:17 -0700

    update for longer session

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: "Super secret secret",
   cookie: {
-    maxAge: 3600000,
+    maxAge: 1800000,
     httpOnly: true,
     secure: false,
     sameSite: "strict",
